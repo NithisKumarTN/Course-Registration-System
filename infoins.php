7973a749488de0a8ccbee3bb3344c2ac221495bd
Nithis Kumar TN <nithiskumar.tn@gmail.com>
2018-12-31 13:25:55 +0530

    Update infoins.php
    Just added a constraint that avoiding of duplicate data before insertion....that's was missing previously...

--- infoins.php
+++ infoins.php
@@ -2,7 +2,12 @@
 <head>
 
 <?php
+
+
+
+
 include_once 'db1.php';
+
 $Name=$_POST['nm'];
 $Address=$_POST['add'];
 $DOB=$_POST['dob'];
@@ -11,23 +16,39 @@ $Education_qualified=$_POST['edu'];
 $Timing=$_POST['time'];
 $Contact_No=$_POST['cont'];
 $Course=$_POST['course'];
-$res=mysqli_query($con,"INSERT  into reg values ('$Name','$Address','$DOB','$Gender','$Education_qualified','$Timing','$Contact_No','$Course')")or die('insertion problem');
+
+$query = "SELECT * FROM `reg` WHERE `Name` = '{$Name}'";
+ 
+$result = mysqli_query($con,$query);
+ 
+if ( mysqli_num_rows ($result ) > 0 )
+{
+    /* Username already exists */
+    echo "<script type='text/javascript'>alert('Name already exists...');</script>";
+    include "register.php";
+}
+else
+{
+	$res=mysqli_query($con,"INSERT  into reg values ('$Name','$Address','$DOB','$Gender','$Education_qualified','$Timing','$Contact_No','$Course')")or die('insertion problem')or die(mysql_error());
+	echo "<script type='text/javascript'>alert('Registered Sucessfully..!!');</script>";
+	include "image.php";
+
+
+}
+
 
 
 if(empty($Name&&$Address&&$DOB&&$Gender&&$Education_qualified&&$Timing&&$Contact_No&&$Course))
 {
+	
 	echo "<script type='text/javascript'>alert('Fillup all the details..!!');</script>";
 	include "register.php";	
 }
 
-else
-{
-	echo "<script type='text/javascript'>alert('Registered Sucessfully..!!');</script>";
-	include "image.php";
-
-}
-
 	
+	
+
+
 
 ?>
 </head>
